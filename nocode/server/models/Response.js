6dda931421724d0fbe49e79a6ad850006ed97ce1
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2025-11-06 16:15:15 +0530

    Delete nocode directory

--- nocode/server/models/Response.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const ResponseSchema = new mongoose.Schema({
-  form: { type: mongoose.Schema.Types.ObjectId, ref: 'Form', required: true },
-  answers: Object,
-  meta: Object
-});
-module.exports = mongoose.model('Response', ResponseSchema);
